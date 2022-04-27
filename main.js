8e2fc482f7f8f3f1c47ff6567b88ee769b8164c0
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-04-27 04:10:17 -0700

    logical structure and styling created for work page

---
+++ main.js
@@ -0,0 +1,9 @@
+const titleApp = document.querySelector("title");
+
+// function updateTitle() {
+//   if (titleApp === "Document") {
+//     document.title = "WORK - ROOTS BEFORE BRANCHES";
+//   } else {
+//     document.title = titleApp;
+//   }
+// }
