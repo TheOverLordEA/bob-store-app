2e1e7aecbd999172543f3f49c3ed2ee929a1910a
Erkhembayar Atarbold <74484951+TheOverLordEA@users.noreply.github.com>
2022-04-28 03:34:45 -0700

    built the journal page

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ const titleApp = document.querySelector("title");
 //     document.title = titleApp;
 //   }
 // }
+
+const pageInfo = [
+  {
+    id: "About-Page",
+    name: "ABOUT — ROOTS BEFORE BRANCHES",
+    category: "HTML",
+  },
+];
+
+// function setTitle () {
+//     for (let index = 0; index < array.length; index++) {
+//         const element = array[index];
+
+//     }
+// }
